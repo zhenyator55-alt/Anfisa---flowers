55760feceb7499b7f78b5af4cc3e96d73303dec5
Torianuk <zhenyator55@gmail.com>
2026-02-11 17:01:01 +0200

    Додано розв’язок задачі

---
+++ src.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+  long long n, m;
+  cout << "Мурзик створює клумбу, яких розмірів вона буде? Задай розміри клумби по ширині: ";
+  cin >> n;
+  cout << "і по висоті: ";
+  cin >> m;
+  
+}
